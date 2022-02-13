a2d87c2a2a4d466b2107b24ff7ac821fb7f24126
Daniel Henri <djosephhenri@gmail.com>
2022-02-14 01:40:51 +0900

    removed zoom control before capturing image for cart

--- src/components/AddToCart.svelte
+++ src/components/AddToCart.svelte
@@ -4,9 +4,11 @@
 
     import { cart } from '../stores/stores.js';
     import { item } from '../stores/stores.js';
+    import { map } from '../stores/stores.js';
 
     // make image of the map 
-    const mapImage = () => htmlToImage.toJpeg(document.getElementById('map'), {quality: 0.50})
+    const mapImage = () => 
+        htmlToImage.toJpeg(document.getElementById('map'), {quality: 0.50})
         .then(function (dataUrl) {
             let img = new Image();
             img.src = dataUrl;
@@ -18,7 +20,9 @@
 
     // add item to cart
     const onAddToCartClick = async () => {
+        $map.removeControl($map.zoomControl);
         await mapImage();
+        $map.addControl($map.zoomControl);
         const itemVal = Object.assign({},$item);
         $cart = [...$cart, itemVal];
     }
